88dbcfc3ce0410283b76956a0277a27a006cafa8
makkov <maxim_86nva@mail.ru>
2024-06-05 21:43:29 +0300

    init project

---
+++ src/main/java/com/example/colvir_demo/ColvirDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.colvir_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColvirDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColvirDemoApplication.class, args);
+	}
+
+}
